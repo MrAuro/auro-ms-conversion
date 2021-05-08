b12e24841256771fd7effc23aae8b659c73a3807
Auro <35087590+MrAuro@users.noreply.github.com>
2021-05-08 17:20:09 -0400

    Added relativeTime

--- index.js
+++ index.js
@@ -1,7 +1,91 @@
 #!/usr/bin/env node
 
-module.exports = {
-    test: () => {
-        console.log('bbbbb');
-    },
-};
+/**
+ * [relativeTime Determines the relative time from miliseconds]
+ * @param  {number}  ms   [miliseconds]
+ * @param  {boolean} long [long string]
+ * @return {string}       [string of the relative time]
+ */
+function relativeTime(ms, long) {
+    let days, hours, minutes, seconds;
+    seconds = Math.floor(ms / 1000);
+
+    minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+
+    hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+
+    days = Math.floor(hours / 24);
+    hours = hours % 24;
+
+    if (long) {
+        let resultArr = [];
+
+        if (days == 1) {
+            days > 0 ? resultArr.push(days + ' day') : '';
+        } else if (days > 1) {
+            days > 0 ? resultArr.push(days + ' days') : '';
+        }
+
+        if (hours == 1) {
+            hours > 0 ? resultArr.push(hours + ' hour') : '';
+        } else if (hours > 1) {
+            hours > 0 ? resultArr.push(hours + ' hours') : '';
+        }
+
+        if (minutes == 1) {
+            minutes > 0 ? resultArr.push(minutes + ' minute') : '';
+        } else if (minutes > 1) {
+            minutes > 0 ? resultArr.push(minutes + ' minutes') : '';
+        }
+
+        if (seconds == 1) {
+            seconds > 0 ? resultArr.push(seconds + ' second') : '';
+        } else if (seconds > 1) {
+            seconds > 0 ? resultArr.push(seconds + ' seconds') : '';
+        }
+
+        // Check if there are only seconds
+        if (days == 0 && hours == 0 && minutes == 0) {
+            return resultArr[0];
+        } else if (resultArr.length == 2) {
+            return `${resultArr[0]} and ${resultArr[1]}`;
+        } else {
+            resultArr[resultArr.length - 1] = `and ${resultArr[resultArr.length - 1]}`;
+
+            return resultArr.join(', ');
+        }
+    } else {
+        let resultArr = [];
+
+        if (days > 1) {
+            days > 0 ? resultArr.push(days + 'd') : '';
+        }
+
+        if (hours > 1) {
+            hours > 0 ? resultArr.push(hours + 'h') : '';
+        }
+
+        if (minutes > 1) {
+            minutes > 0 ? resultArr.push(minutes + 'm') : '';
+        }
+
+        if (seconds > 1) {
+            seconds > 0 ? resultArr.push(seconds + 's') : '';
+        }
+
+        // Check if there are only seconds
+        if (days == 0 && hours == 0 && minutes == 0) {
+            return resultArr[0];
+        } else if (resultArr.length == 2) {
+            return `${resultArr[0]} and ${resultArr[1]}`;
+        } else {
+            resultArr[resultArr.length - 1] = `and ${resultArr[resultArr.length - 1]}`;
+
+            return resultArr.join(', ');
+        }
+    }
+}
+
+module.exports = { relativeTime };
---
+++ readme.md
@@ -0,0 +1,22 @@
+# auro-ms-conversion
+
+This is a small milisecond conversion package, that converts miliseconds into human readable text.
+
+The `relativeTime` function has two parameters:
+
+`ms` which is the miliseconds you want to convert
+
+`long` which is whether you want the string to be like: "10d" or "10 days" Defaults to false
+
+Usage:
+
+```js
+// *the package name might be different*
+const msConversion = require('auro-ms-conversion');
+
+console.log(msConversion.relativeTime(8123751236));
+// 94d, 35m, and 51s
+
+console.log(msConversion.relativeTime(8123751236, true));
+// 94 days, 35 minutes, and 51 seconds
+```
